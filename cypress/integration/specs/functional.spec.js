c4b506136a2850c385b4133876c970575793562c
William Pereira <william-pereira@hotmail.com>
2022-06-07 18:33:05 -0300

    update-tests

--- cypress/integration/specs/functional.spec.js
+++ cypress/integration/specs/functional.spec.js
@@ -18,7 +18,7 @@ describe ('logged in user', () => {
 
 it('Changing an account', () => {
   cy.acessarMenuConta()
-  cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).should('exist').click()
+  cy.xpath(loc.CONTAS.XP_BTN_ALTERAR('Uma conta de Internet Will')).should('exist').click()
   cy.get(loc.CONTAS.NOME)
       .clear()
       .type('Uma conta de Internet Will ALTERADA')
@@ -39,16 +39,25 @@ it('Should create a transaction', () => {
   cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Conta de Internet')
   cy.get(loc.MOVIMENTACAO.VALOR).type('100')
   cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Will')
+  cy.get(loc.MOVIMENTACAO.CONTA).select('Uma conta de Internet Will ALTERADA')
   cy.get(loc.MOVIMENTACAO.STATUS).click()
   cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
-
   cy.get(loc.MESSAGE).should('contain', 'sucesso')
   cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
-  cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+  cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO('Conta de Internet','100')).should('exist')
 
 })
-it('Should get balance', () => {
-  cy.get(loc.MENU.HOME).click()
-  cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para alterar')).should('contain', '100')
-})
+  it('Should get balance', () => {
+    cy.get(loc.MENU.HOME).click()
+    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Uma conta de Internet Will ALTERADA')).should('contain', '100')
+  })
+
+  it('Should remove a transaction', () => {
+    cy.get(loc.MENU.EXTRATO).click()
+    cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Conta de Internet')).click()
+    cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+
+  })
+
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -9,23 +9,26 @@ const locators = {
       SETTINGS: '[data-test="menu-settings"]',
       CONTAS: '[href="/contas"]',
       RESET: '[href="/reset"]',
-      MOVIMENTACAO: '[data-test="menu-movimentacao"]'
+      MOVIMENTACAO: '[data-test="menu-movimentacao"]',
+      EXTRATO: "[data-test=menu-extrato]"
   }, 
   CONTAS: {
     NOME: '[data-test="nome"]',
     BTN_SALVAR: '.btn',
-    XP_BTN_ALTERAR: '//tbody/tr/td[text()="Uma conta de Internet Will"]'
+    XP_BTN_ALTERAR: nome => `//tbody/tr/td[text()="${nome}"]`
   },
   MOVIMENTACAO: {
     DESCRICAO: '#descricao',
     VALOR: '[data-test="valor"]',
     INTERESSADO: '#envolvido',
     BTN_SALVAR: '.btn-primary',
-    STATUS: '[data-test=status]'
+    STATUS: '[data-test=status]',
+    CONTA: '[data-test=conta]'
   },
   EXTRATO: {
     LINHAS: '.list-group > li',
-    XP_BUSCA_ELEMENTO: "//span[contains(.,'Conta de Internet')]/following-sibling::small[contains(.,'100')]"
+    XP_BUSCA_ELEMENTO: (nome, qtde) => `//span[contains(.,'${nome}')]/following-sibling::small[contains(., '${qtde}')]`,
+    FN_XP_REMOVER_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='far fa-trash-alt']`
   },
   SALDO: {
     FN_XP_SALDO_CONTA: nome => `//td[contains(.,'${nome}')]/../td[2]`
